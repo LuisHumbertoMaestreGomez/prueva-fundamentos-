45bf1a066272021f2e3727b475432bfca6991d6a
LuisHumbertoMaestreGomez <https://github.com/LuisHumbertoMaestreGomez/prueva-fundamentos-.git>
2024-01-24 13:03:05 -0500

    ejercicio2

---
+++ ejercicio2.py
@@ -0,0 +1,6 @@
+base = float(input("ingrese la base de su triangulo"))
+altura = float(int("ingrese la altura de su triangulo"))
+
+area = (base * altura)/2
+
+print(f"el area de su triangulo es {area}")
