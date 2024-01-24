e79da75f6467591952bf09ec3013388722de2d96
LuisHumbertoMaestreGomez <https://github.com/LuisHumbertoMaestreGomez/prueva-fundamentos-.git>
2024-01-24 12:46:10 -0500

    ejercicio1

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+vol1 = float(input("ingrese un valor "))
+vol2 = float(input("ingrese un valor "))
+vol3 = float(input("ingrese un valor "))
+vol4 = float(input("ingrese un valor "))
+vol5 = float(input("ingrese un valor "))
+
+rta = (vol1 + vol2 + vol3 + vol4 + vol5)/5
+
+if rta > 220:
+    print ("!!ALTO VOLTAJE!!")
+
+else:
+    print("VOLTAJE CORRECTO :3")
